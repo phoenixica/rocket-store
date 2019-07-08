a9e37422e8c6a6ce4ce37e4d5b94b20478e9903c
samahab77 <s.bagido@gmail.com>
2019-07-08 21:22:12 +0300

    trying add slide img and fix missing file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React,{ Component } from 'react';
 import Header from './music/Header';
 
+const slideImages = [
+    'images/img1.png',
+    'images/img2.png.jpg',
+    'images/img5.png.jpg'
+];
+
+const properties = {
+    duration: 1000,
+    transitionDuration: 500,
+    infinite: true,
+    indicators: true,
+    arrows: true,
+    onChange: (oldIndex, newIndex) => {
+        console.log(`slide transition from ${oldIndex} to ${newIndex}`);
+    }
+}
 
 const slideImages = [
     'images/img1.png',
