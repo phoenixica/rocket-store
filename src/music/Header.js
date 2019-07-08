adc5b6d6cfec605e4e09c33c9c40d33385843edc
danyahalfageh <dalfakih@yahoo.com>
2019-07-08 12:46:54 +0300

    my changes

--- src/music/Header.js
+++ src/music/Header.js
@@ -2,7 +2,7 @@ import React,{ Component } from 'react';
 import Nav from './Nav';
 import About from './About';
 import Products from './Products';
-import Seller from './Seller';
+// import Seller from './Seller';
 import Home from './Home';
 import { Link , Route} from 'react-router-dom';
 
@@ -17,12 +17,12 @@ return (
             <li><Link to='/search'>Search Albums</Link></li>
             <li><Link to='/albums'>All Albums</Link></li>
             <li><Link to='/about'>About</Link></li>
-            <li><Link to='/sell'>Sell Your Album</Link></li>
+           {/* // <li><Link to='/sell'>Sell Your Album</Link></li> */}
         </ul>
         <Route exact path='/' component={Home}/>
         <Route exact path='/about' component={About}/>
         <Route exact path='/albums' component={Products}/>
-        <Route exact path='/sell' component={Seller}/>
+        {/* <Route exact path='/sell' component={Seller}/> */}
         <Nav />
     </div>
 );
--- src/music/Products.js
+++ src/music/Products.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import axios from 'axios';
 import List from './List';
-import { Link } from 'react-router-dom';
 const LFAPI_KEY='5fbde430b114ee63de9bbea86b2bf8cb';
 
 export default class Product extends React.Component 
 {
-      state={
-        album:[],
-        search:'a'
-      }
+    state={
+      album:[],
+      search:'a'
+    }
   componentDidMount() {
       axios({
         url:`http://ws.audioscrobbler.com/2.0/?method=album.search&album=${this.state.search}&api_key=${LFAPI_KEY}&format=json`,
@@ -26,29 +25,27 @@ export default class Product extends React.Component
       }) 
   }
   render()
-    {
-    
-      const albums = this.state.album.map((album,index) =>
-              <List 
-                key={index}
-                name={album.name}
-                artist={album.artist}
-                image={album.image[3]['#text']}
-              />
-            )
+  {
+    const albums = this.state.album.map((album,index) =>
+      <List 
+        key={index}
+        name={album.name}
+        artist={album.artist}
+        image={album.image[3]['#text']}
+      />
+    )
 
-        return (
-              <div className="App">
-                <div>
-                  <header>Record Store</header>
-                </div>
-                <ul>
-                {albums}
-                </ul>
-              </div>
-        );
-    }
-
+    return (
+      <div className="App">
+        <div>
+          <header>Record Store</header>
+        </div>
+        <ul>
+        {albums}
+        </ul>
+      </div>
+    );
+  }
 }
 
 
